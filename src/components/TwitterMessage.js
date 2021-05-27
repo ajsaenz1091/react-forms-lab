31b01c317981dafd26a9ea954fc5af7a83d8d81d
Alberto <ajsaenz1091@gmail.com>
2021-05-27 18:55:41 -0400

    Completed Assignment

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,39 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      message: ''
+
+    };
   }
 
+  handleChange = (e) => {
+    let name = e.target.name;
+    let val = e.target.value;
+    this.setState({[name]: val})
+  }
+
+  // countMessage = () => {
+  //   if(this.state.message <= this.props.maxChars){
+  //     this.setState({totalChars: this.state.totalChars + 1})
+  //     return this.state.totalChars
+  //   }else{
+  //     return <p> Too many Characters. Try again! </p>
+  //   }
+  // }
+
   render() {
+    let charNum = this.props.maxChars - this.state.message.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input 
+        onChange={this.handleChange}
+        type="text" 
+        name="message" 
+        id="message"
+        value={this.state.message} />
+        {charNum} {/*ternary operator would also work here*/} {/*{this.state.message && charNum}*/}
       </div>
     );
   }
